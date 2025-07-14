002f9f57ea3ecef50b28ce5bc5cdddd3b73ab55c
Felix Ombongi <105150658+OmbongiFelix@users.noreply.github.com>
2025-07-14 16:57:48 +0300

    Update settings.py
    added missing colon in host in database

--- afridata/afridata/settings.py
+++ afridata/afridata/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME':env('DB_NAME', default='afridata'),
         'USER':env('DB_USER'),
         'PASSWORD':env('DB_PASSWORD'),
-        'HOST':env('DB_HOST' default='localhost'),
+        'HOST':env('DB_HOST', default='localhost'),
         'PORT':env('DB_PORT', default='3306'),
         'OPTIONS':{
             'charset':'utf8mb4',
