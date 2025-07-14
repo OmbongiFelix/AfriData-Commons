3a6324aa6bcc07f4905e7fc6d642c3e18dbd1300
Felix Ombongi <105150658+OmbongiFelix@users.noreply.github.com>
2025-07-14 18:40:49 +0300

    Update urls.py

--- afridata/afridata/urls.py
+++ afridata/afridata/urls.py
@@ -4,8 +4,7 @@ from schema_graph.views import Schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('home.urls')),
-    path('', lambda request: HttpResponse("Homepage is working ✅")),
+    path('', include('home.urls')),
     path('api/', include('api.urls')),
     path('accounts/', include('accounts.urls')),
     path('dataset/', include('dataset.urls')),
