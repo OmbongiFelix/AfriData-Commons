485fbb884b4e3ef315fda93cfe3535a61d321ec6
OmbongiFelix <fombongi11@gmail.com>
2025-07-23 14:02:22 +0300

    add some new urls and remove others

--- afridata/dataset/urls.py
+++ afridata/dataset/urls.py
@@ -1,17 +1,25 @@
+#dataset/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Dataset listing and browsing
     path('', views.dataset_list, name='dataset_list'),
-    #path('dataset/<int:dataset_id>/', views.render_dataset, name='dataset_page'),
+    
+    # Dataset detail and related pages
     path('dataset/<int:dataset_id>/', views.dataset_detail, name='dataset_detail'),
     path('dataset/<int:dataset_id>/preview/', views.dataset_preview, name='dataset_preview'),
     path('dataset/<int:dataset_id>/download/', views.download_dataset, name='download_dataset'),
     path('dataset/<int:dataset_id>/comments/', views.dataset_comments, name='dataset_comments'),
-    #path('dataset/<int:dataset_id>/comment/post/', views.post_comment, name='post_comment'),
+    
+    # Comment functionality
     path('comment/<int:dataset_id>/post/', views.post_comment, name='post_comment'),
     path('comment/<int:comment_id>/upvote/', views.upvote_comment, name='upvote_comment'),
+    
+    # Dataset upload
     path('datasets/upload/', views.upload_dataset, name='upload_dataset'),
-    #path('datasets/upload/ajax/', views.upload_dataset_ajax, name='upload_dataset_ajax'),
+    
+    # User dashboard and profile pages
+    path('dashboard/', views.user_dashboard, name='user_dashboard'),
+    path('tokens/history/', views.token_history, name='token_history'),
 ]
-
