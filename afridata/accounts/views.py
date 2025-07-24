62244d0c09450840024e6602c8b23cafbb663c56
OmbongiFelix <fombongi11@gmail.com>
2025-07-24 22:05:50 +0300

    remove previous daraja related views

--- afridata/accounts/views.py
+++ afridata/accounts/views.py
@@ -27,13 +27,6 @@ from accounts.models import CustomUser, LoginAttempt, TokenPurchase, UserProfile
 from dataset.forms import DatasetUploadForm
 from dataset.models import Comment, Dataset, Download, PremiumPurchase, Referral, TokenTransaction
 
-import requests
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from requests.auth import HTTPBasicAuth
-
-from .credentials import LipanaMpesaPpassword, MpesaAccessToken, LipanaMpesaPpasswor
-
 User = get_user_model()
 
 
@@ -733,55 +726,3 @@ def referrals_view(request):
     }
     
     return render(request, 'accounts/referrals.html', context)
-
-@login_required
-def token_purchase(request):
-    return render(request, 'accounts/token_purchase.html')
-
-
-
-def token(request):
-    consumer_key = 'FvFAsAmUt3KiVfuvAx0H2A8Lzg3VOS5IhyQ35ZgIBwdYVTW7'
-    # 8CDXKCA9OqHYQ18CcUGk90roZ88YdIG3Rpy9khsld8lFFlxx
-    consumer_secret = 'CYycDezeZJ2X5tYLYcG4fF9bCiFFZPyycGJGppKEY0hVaMzW2gU6FJYFzaWOcJuy'
-    # aaP9pFa3Tzi1sGzUE1Ycj2ssbtDIYqU9hbwRCgAMwu8OnzVz3N3dsUXcoUxAu6Oy
-    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
-    
-
-    r = requests.get(api_URL, auth=HTTPBasicAuth(
-        consumer_key, consumer_secret))
-    mpesa_access_token = json.loads(r.text)
-    validated_mpesa_access_token = mpesa_access_token["access_token"]
-
-    return render(request, 'token.html', {"token":validated_mpesa_access_token})
-
-def pay(request):
-   return render(request, 'pay.html')
-
-
-
-def stk(request):
-    if request.method =="POST":
-        phone = request.POST['phone']
-        amount = request.POST['amount']
-        access_token = MpesaAccessToken.validated_mpesa_access_token
-        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
-        headers = {"Authorization": "Bearer %s" % access_token}
-        request = {
-            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
-            "Password": LipanaMpesaPpassword.decode_password,
-            "Timestamp": LipanaMpesaPpassword.lipa_time,
-            "TransactionType": "CustomerPayBillOnline",
-            "Amount": amount,
-            "PartyA": phone,
-            "PartyB": LipanaMpesaPpassword.Business_short_code,
-            "PhoneNumber": phone,
-            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
-            "AccountReference": "eMobilis",
-            "TransactionDesc": "Web Development Charges"
-        }
-        response = requests.post(api_url, json=request, headers=headers)
-        return HttpResponse("Success")
-
-
-
