992bc64b6c08014e5e0b23b5aa1748fd3a831aa9
JAK Softwares <josephamuyunzu1979@gmail.com>
2025-07-25 09:04:15 +0300

    Worked on mpesa integration to work (#23)
    Co-authored-by: jaksoftwares <jaksoftwares05@gmail.com>

--- afridata/afridata/settings.py
+++ afridata/afridata/settings.py
@@ -26,11 +26,17 @@ if os.environ.get('GITHUB_ACTIONS') != 'true':
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=True)
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'https://afridata-commons-1.onrender.com/']
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'https://afridata-commons-1.onrender.com/']
+
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'https://afridata-commons-1.onrender.com/']
+
+# ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1', 'https://afridata-commons-1.onrender.com/']
+
+# MPESA_CALLBACK_URL = "https://748f2b6a94e5.ngrok-free.app/mpesa/callback/"
+
 
 
 
@@ -211,5 +217,6 @@ GRAPH_MODELS = {
         'all_applications': True,
         'graph_models': True,
     }
+
 
 
